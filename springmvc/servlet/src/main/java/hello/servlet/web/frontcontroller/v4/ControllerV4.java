b257f3d92aead86062f519310716ac199bdabd22
Jong Hak Kim <whdgkr9070@naver.com>
2024-06-04 18:24:50 +0900

    Spring MVC Part.1 Study

---
+++ springmvc/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,14 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import java.util.Map;
+
+
+public interface ControllerV4 {
+
+    /**
+     * @param ParamMap
+     * @param model
+     * @return
+     */
+    String process(Map<String, String> ParamMap, Map<String, Object> model);
+}
