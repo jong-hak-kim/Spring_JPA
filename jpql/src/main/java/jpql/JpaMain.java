3371c6d7c93a628236e1651734c53e6dc6867318
Jong Hak Kim <whdgkr9070@naver.com>
2024-06-20 11:18:21 +0900

    Object Oriented Query Language 2 - Intermediate Grammar

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -29,28 +29,30 @@ public class JpaMain {
             Member member1 = new Member();
             member1.setUsername("회원1");
             member1.setTeam(teamA);
+            member1.setAge(0);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("회원2");
             member2.setTeam(teamA);
+            member2.setAge(0);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("회원3");
             member3.setTeam(teamB);
+            member3.setAge(0);
             em.persist(member3);
 
-            em.flush();
+            //FLUSH 자동 호출
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+
             em.clear();
+            
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember = " + findMember.getAge());
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
-
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
 
             tx.commit();
         } catch (Exception e) {
