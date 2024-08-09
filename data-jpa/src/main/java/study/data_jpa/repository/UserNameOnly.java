826cf193d57b1c29fe477145c066dc0c0e054646
Jong Hak Kim <whdgkr9070@naver.com>
2024-08-09 12:01:34 +0900

    Query By Example & Projections

---
+++ data-jpa/src/main/java/study/data_jpa/repository/UserNameOnly.java
@@ -0,0 +1,9 @@
+package study.data_jpa.repository;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public interface UserNameOnly {
+
+    @Value("#{target.username + ' ' + target.age}")
+    String getUsername();
+}
