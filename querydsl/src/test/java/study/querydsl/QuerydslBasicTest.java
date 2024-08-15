a846dc8c6a7456af8ea40a8553133437d21c855c
Jong Hak Kim <whdgkr9070@naver.com>
2024-08-15 20:14:52 +0900

    Basic join

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -38,10 +38,10 @@ public class QuerydslBasicTest {
         em.persist(teamB);
 
         Member member1 = new Member("member1", 10, teamA);
-        Member member2 = new Member("member1", 20, teamA);
+        Member member2 = new Member("member2", 20, teamA);
 
-        Member member3 = new Member("member1", 30, teamB);
-        Member member4 = new Member("member1", 40, teamB);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
 
         em.persist(member1);
         em.persist(member2);
@@ -246,11 +246,45 @@ public class QuerydslBasicTest {
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35); //(30 + 40) / 2
 
+    }
+
+
+    //팀 A에 소속된 모든 회원
+    @Test
+    public void join() throws Exception {
+        //given
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+
+    }
+
+    //세타 조인
+    //회원의 이름이 팀 이름과 같은 회원 조회
+
+    @Test
+    public void theta_join() throws Exception {
+        //given
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
 
         //when
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
 
         //then
-
+        assertThat(result).extracting("username")
+                .containsExactly("teamA", "teamB");
     }
 
 }
