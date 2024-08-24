b20d0fa92c64fb1f59d44473cfb08417822722f5
Jong Hak Kim <whdgkr9070@naver.com>
2024-08-24 21:48:16 +0900

    Dynamic Queries and Performance Optimization Inquiries - Using the Where clause parameter

--- querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -61,13 +61,13 @@ class MemberJpaRepositoryTest {
         em.persist(member4);
 
         MemberSearchCondition condition = new MemberSearchCondition();
-//        condition.setAgeGoe(20);
-//        condition.setAgeLoe(40);
-//        condition.setTeamName("teamB");
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
 
-        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+        List<MemberTeamDto> result = memberJpaRepository.search(condition);
 
-        assertThat(result).extracting("username").containsExactly("member3", "member4");
+        assertThat(result).extracting("username").containsExactly("member4");
 
     }
 
